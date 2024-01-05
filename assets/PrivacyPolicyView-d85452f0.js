15fb2029867bad14a8fb2327c344ee2c029ed97b
HG\B1208141 <tecforce@hwa-guan.com.tw>
2024-01-05 17:43:48 +0800

    complete send mail procdure

---
+++ assets/PrivacyPolicyView-d85452f0.js
@@ -0,0 +1,1 @@
+import{d as t,o as i,c as n,a as e,i as o,Q as a}from"./index-64be13f7.js";const s={class:"serviceMainContainer"},r=e("div",null,null,-1),c={class:"TOSBody"},d=e("div",{class:"TOSTitle"},[e("h3",null,"隱私權政策")],-1),l=e("div",null,null,-1),u=t({__name:"PrivacyPolicyView",setup(p){return(_,m)=>(i(),n("div",s,[r,e("div",c,[d,o(a)]),l]))}});export{u as default};
